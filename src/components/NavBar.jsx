0c84e0db441a034f9f6d778b903bda8b9cee08e5
Ernest HAGUMA KWIZERA <kwizeraernez@gmail.com>
2025-04-03 21:21:41 +0200

    navigations and footer

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-white text-lg font-bold">SKAFF INVEST R GROUP</div>
+                <ul className="flex space-x-4">
+                    <li><a href="#" className="text-white hover:text-gray-400">Home</a></li>
+                    <li><a href="#" className="text-white hover:text-gray-400">About</a></li>
+                    <li><a href="#" className="text-white hover:text-gray-400">Services</a></li>
+                    <li><a href="#" className="text-white hover:text-gray-400">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+// src/layouts/MainLayout.jsx
+import React from 'react';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
